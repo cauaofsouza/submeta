dd6aa878c99ad47c2a46e726a9f55b52501376f4
cauaofsouza <cauasantos064@gmail.com>
2026-07-20 20:47:59 -0300

    feat(ProgramaDeExtensao): refatora view de editais para coordenador para mostrar programas de extensao
    - backend ja resolvido em CoordenadorComissaoController

--- resources/views/coordenadorComissao/editais.blade.php
+++ resources/views/coordenadorComissao/editais.blade.php
@@ -9,11 +9,11 @@
     <div class="row p-b-5" >
 
       <div class="col-sm-4" style="float: start;">
-        <h3 class="title-table">Meus Editais</h3> 
+        <h3 class="title-table">Meus Editais</h3>
       </div>
       <div class="col-sm-8 align-text-top aling-end ">
           <a href="{{route('evento.criar')}}" class="btn btn-info" style="float: right;">Criar Edital</a>
-        <a href="{{route('evento.criarProgramaExtensao')}}" class="btn btn-info" style="float: right; margin-right:20px">Criar Programa de Extensão</a>
+        <a href="{{route('programa.criar')}}" class="btn btn-info" style="float: right; margin-right:20px">Criar Programa de Extensão</a>
 
       </div>
 
@@ -33,60 +33,83 @@
   @endif
   <table class="table table-bordered">
     <thead>
-      <tr>   
-        <th scope="col">Nome do Edital</th>
+      <tr>
+        <th scope="col">Nome do Edital/Programa</th>
         <th scope="col">Data de Criação</th>
         <th scope="col">Opção</th>
       </tr>
     </thead>
     <tbody>
-      @foreach ($eventos as $evento)
-        <tr>
-          <td>
-            <a href="{{  route('evento.visualizar',['id'=>$evento->id])  }}" class="visualizarEvento">
-                {{ $evento->nome }}
+    @foreach ($listagem as $item)
+      <tr>
+        <td>
+          <a href="{{ $item['tipo'] === 'evento'
+                ? route('evento.visualizar', ['id' => $item['id']])
+                : route('programa.visualizar', ['id' => $item['id']]) }}"
+             class="visualizarEvento">
+            {{ $item['nome'] }}
+          </a>
+        </td>
+        <td>{{ date('d/m/Y \à\s H:i\h', strtotime($item['created_at'])) }}</td>
+        <td>
+          <div class="btn-group dropright dropdown-options">
+            <a id="options" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <img src="{{ asset('img/icons/ellipsis-v-solid.svg') }}" style="width:8px">
             </a>
-          </td>
-          <td>{{ date('d/m/Y \à\s H:i\h', strtotime($evento->created_at)) }}</td>
-          <td>
-            <div class="btn-group dropright dropdown-options">
-                <a id="options" class="dropdown-toggle " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                     <img src="{{asset('img/icons/ellipsis-v-solid.svg')}}" style="width:8px"> 
+
+            {{-- DROPDOWN EVENTO --}}
+            @if($item['tipo'] === 'evento')
+              <div class="dropdown-menu">
+                <a href="{{ route('evento.editar', ['id' => $item['id']]) }}" class="dropdown-item text-center">
+                  Editar Edital
                 </a>
-                <div class="dropdown-menu">
-                  <a href="{{ route('evento.editar', ['id' => $evento->id]) }}" class="dropdown-item text-center">
-                    Editar Edital
-                  </a>
+                <hr class="dropdown-hr">
+                <a href="{{ route('admin.analisar', ['evento_id' => $item['id']]) }}" class="dropdown-item text-center">
+                  Visualizar Projetos
+                </a>
+                <hr class="dropdown-hr">
+                <a href="{{ route('admin.atribuir', ['evento_id' => $item['id']]) }}" class="dropdown-item text-center">
+                  Atribuir Avaliadores
+                </a>
+                <hr class="dropdown-hr">
+                <a href="{{ route('admin.pareceres', ['evento_id' => $item['id']]) }}" class="dropdown-item text-center">
+                  Visualizar Pareceres
+                </a>
+                @if($item['tipoAvaliacao'] != 'link')
                   <hr class="dropdown-hr">
-                  <a href="{{route('admin.analisar', ['evento_id' => $evento->id])}}" class="dropdown-item text-center">
-                    Visualizar Projetos
+                  <a href="{{ route('admin.showResultados', ['evento_id' => $item['id']]) }}" class="dropdown-item text-center">
+                    Resultados
                   </a>
-                  <hr class="dropdown-hr">
-                  <a href="{{route('admin.atribuir', ['evento_id' => $evento->id])}}" class="dropdown-item text-center">
-                      Atribuir Avaliadores
-                  </a>
-                  <hr class="dropdown-hr">
-                  <a href="{{route('admin.pareceres', ['evento_id' => $evento->id])}}" class="dropdown-item text-center">
-                      Visualizar Pareceres
-                  </a>
-                  @if($evento->tipoAvaliacao != "link")
-                  <hr class="dropdown-hr">
-                  <a href="{{route('admin.showResultados', ['evento_id' => $evento->id])}}" class="dropdown-item text-center">
-                      Resultados
-                  </a>
-                  @endif
-                  <hr class="dropdown-hr">
-                    <!-- Button trigger modal -->
-                    <button type="button" class="dropdown-item dropdown-item-delete text-center" data-toggle="modal" data-target="#exampleModal{{ $evento->id }}">
-                      <img src="{{asset('img/icons/logo_lixeira.png')}}" alt=""> Deletar
-                    </button>
-
-
+                @endif
+                <hr class="dropdown-hr">
+                <button type="button" class="dropdown-item dropdown-item-delete text-center"
+                        data-toggle="modal" data-target="#exampleModal{{ $item['id'] }}">
+                  <img src="{{ asset('img/icons/logo_lixeira.png') }}" alt=""> Deletar
+                </button>
               </div>
-            </div>
-          </td>
-        </tr>
-      @endforeach
+
+              {{-- DROPDOWN PROGRAMA DE EXTENSÃO --}}
+            @else
+              <div class="dropdown-menu">
+                <a href="{{ route('programa.editar', ['id' => $item['id']]) }}" class="dropdown-item text-center">
+                  Editar Programa
+                </a>
+                <hr class="dropdown-hr">
+                <a href="{{ route('admin.analisar-trabalhos-programa-extensao', ['id' => $item['id']]) }}" class="dropdown-item text-center">
+                  Visualizar Projetos
+                </a>
+                <hr class="dropdown-hr">
+                <button type="button" class="dropdown-item dropdown-item-delete text-center"
+                        data-toggle="modal" data-target="#exampleModal{{ $item['id'] }}">
+                  <img src="{{ asset('img/icons/logo_lixeira.png') }}" alt=""> Deletar
+                </button>
+              </div>
+            @endif
+
+          </div>
+        </td>
+      </tr>
+    @endforeach
     </tbody>
   </table>
 </div>
@@ -95,6 +118,6 @@
 
 @section('javascript')
 <script>
-  
+
 </script>
 @endsection
